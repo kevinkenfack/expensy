3a5b54cc80609805bb2b28ca6ba336ff996b8fcb
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-12-20 10:22:13 +0100

    fix clerk importation

--- app/api/stats/route.ts
+++ app/api/stats/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/db";
 
 export async function POST(req: Request) {
